17b917cdd2411632c97b788f2d236cc7e6acab10
Venkata Vinay Sunkara <146333955+vinaysunkara20@users.noreply.github.com>
2026-05-03 01:15:46 +0530

    Update photo.js

--- photo.js
+++ photo.js
@@ -50,3 +50,37 @@ viewer.onclick = (e) => {
     viewer.style.display = "none";
   }
 };
+
+const viewer = document.getElementById("viewer");
+const viewerImg = document.getElementById("viewerImg");
+const closeBtn = document.getElementById("closeBtn");
+
+let currentIndex = 0;
+
+// click image (works for all 447 images)
+document.querySelector(".gallery").addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    const images = document.querySelectorAll(".gallery img");
+
+    currentIndex = Array.from(images).indexOf(e.target);
+    viewer.style.display = "flex";
+    viewerImg.src = e.target.src;
+  }
+});
+
+// close
+closeBtn.onclick = () => viewer.style.display = "none";
+
+// next
+function nextImg() {
+  const images = document.querySelectorAll(".gallery img");
+  currentIndex = (currentIndex + 1) % images.length;
+  viewerImg.src = images[currentIndex].src;
+}
+
+// prev
+function prevImg() {
+  const images = document.querySelectorAll(".gallery img");
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  viewerImg.src = images[currentIndex].src;
+}
